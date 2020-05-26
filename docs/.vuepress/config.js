651adddaf0d80fe37b8f664e113a069f55da1d38
lrz <1414556676@qq.com>
2020-05-26 18:09:55 +0800

    readme,docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	title: 'Luckysheet文档',
+	description: 'Luckysheet 配置文档/API/教程',
+	base: '/LuckysheetDocs/',
+	themeConfig: {
+		logo: '/img/logo.png',
+		// 页面滚动
+		smoothScroll: true,
+		// 导航栏
+		nav: [
+			{ text: '首页', link: '/' },
+			{ text: '指南', link: '/guide/' },
+			{ text: '演示', link: 'https://mengshukeji.github.io/LuckysheetDemo/' },
+			{ text: 'Github', link: 'https://github.com/mengshukeji/Luckysheet' },
+		],
+		// 侧边栏 
+		sidebar: {
+			'/guide/': [
+			  '',
+			  'config',
+			  'feature'
+			],
+		  },
+		// 仓库地址
+		repo: 'mengshukeji/Luckysheet',
+		// 仓库链接文字
+		repoLabel: '在 GitHub 上编辑此页',
+		// 仓库的文档目录 
+		docsDir: 'docs',
+	},
+}
