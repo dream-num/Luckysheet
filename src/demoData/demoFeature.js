3baf93cdb57238ee73ace3e7b9bca142fad81584
mengshukeji <msoffice2@126.com>
2020-11-26 19:20:49 +0800

    feat(demo): tool function
    Add getRequest function

--- src/demoData/demoFeature.js
+++ src/demoData/demoFeature.js
@@ -22,8 +22,27 @@
     
     supportButton()
 
+    /**
+     * Get url parameters
+     */
+    function getRequest() {
+      var url = window.location.search; //获取url中"?"符后的字串
+      var theRequest = new Object();
+      if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+           
+          theRequest[strs[i].split("=")[0]]=decodeURI(strs[i].split("=")[1]);
+           
+        }
+      }
+      return theRequest;
+    }
+
     window.luckysheetDemoUtil = {
-        language:language
+        language:language,
+        getRequest:getRequest
     }
     
 })()
