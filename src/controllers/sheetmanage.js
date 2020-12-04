5e81893f23e4bd348201b43190e94bced5a89f16
linen1794 <30710337+linen1794@users.noreply.github.com>
2020-12-04 15:57:49 +0800

    Update sheetmanage.js
    tooltip of the return button didn't work

--- src/controllers/sheetmanage.js
+++ src/controllers/sheetmanage.js
@@ -746,7 +746,7 @@ const sheetmanage = {
         luckysheetcreatedom(colwidth, rowheight, data, menu, title);
 
         setTimeout(function () {
-            tooltip.createHoverTip("#luckysheet_info_detail" ,".luckysheet_info_detail_title, .luckysheet_info_detail_input, .luckysheet_info_detail_update");
+            tooltip.createHoverTip("#luckysheet_info_detail" ,".luckysheet_info_detail_back, .luckysheet_info_detail_input, .luckysheet_info_detail_update");
             tooltip.createHoverTip("#luckysheet-wa-editor" ,".luckysheet-toolbar-menu-button, .luckysheet-toolbar-button, .luckysheet-toolbar-combo-button");
 
             Store.luckysheetTableContentHW = [
