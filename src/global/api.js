6324431f6ba73232da1d78ca954de421f3fd00f1
CN\wuwx26 <wuwx26@gdii-yueyun.com>
2021-02-02 10:55:41 +0800

    重置sheetmanage.mergeCalculationSheet = {}

--- src/global/api.js
+++ src/global/api.js
@@ -6679,6 +6679,7 @@ export function updataSheet (options = {}) {
         $("#luckysheet-modal-dialog-slider-pivot").hide();
         luckysheetsizeauto(false);
     }
+    sheetmanage.mergeCalculationSheet = {}
     sheetmanage.mergeCalculation(file["index"]);
     sheetmanage.setSheetParam();
     setTimeout(function () {
