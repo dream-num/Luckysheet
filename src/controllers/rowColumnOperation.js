627183f571976335433798667ba26657de0d8dbc
zhangsimu <zhangsimu1992@163.com>
2021-03-25 08:29:53 +0800

    feat(controllers/rowcolumnoperation.j): youjian add he delete hook hanshu,api charu
    xiangxiacharu he xiangyoucharu

--- src/controllers/rowColumnOperation.js
+++ src/controllers/rowColumnOperation.js
@@ -4,6 +4,7 @@ import luckysheetPostil from './postil';
 import imageCtrl from './imageCtrl';
 import menuButton from './menuButton';
 import server from './server';
+import method from '../global/method';
 import { selectHightlightShow, luckysheet_count_show,selectHelpboxFill } from './select';
 import { 
     getObjType, 
@@ -1103,7 +1104,10 @@ export function rowColumnOperationInitial(){
         }
 
         let st_index = Store.luckysheet_select_save[0][Store.luckysheetRightHeadClickIs][0];
-        luckysheetextendtable(Store.luckysheetRightHeadClickIs, st_index, value, "lefttop");
+		if(!method.createHookFunction("rowInsertBefore",  st_index, value, "lefttop")){ 
+			return; 
+		}
+		luckysheetextendtable(Store.luckysheetRightHeadClickIs, st_index, value, "lefttop");
     });
 
 
@@ -1117,6 +1121,9 @@ export function rowColumnOperationInitial(){
         }
 
         let st_index = Store.luckysheet_select_save[0].row[0];
+		if(!method.createHookFunction("rowInsertBefore",  st_index, 1, "lefttop")){ 
+			return; 
+		}
         luckysheetextendtable('row', st_index, 1, "lefttop");
 
 
@@ -1307,6 +1314,9 @@ export function rowColumnOperationInitial(){
         }
 
         let st_index = Store.luckysheet_select_save[0][Store.luckysheetRightHeadClickIs][1];
+		if(!method.createHookFunction("rowInsertBefore",  st_index, value, "rightbottom")){
+			return; 
+		}
         luckysheetextendtable(Store.luckysheetRightHeadClickIs, st_index, value, "rightbottom");
     });
 
@@ -1453,7 +1463,10 @@ export function rowColumnOperationInitial(){
 
         let st_index = Store.luckysheet_select_save[0][Store.luckysheetRightHeadClickIs][0], 
             ed_index = Store.luckysheet_select_save[0][Store.luckysheetRightHeadClickIs][1];
-        luckysheetdeletetable(Store.luckysheetRightHeadClickIs, st_index, ed_index);
+        if(!method.createHookFunction("rowDeleteBefore", st_index, ed_index)){
+        	return; 
+        }
+		luckysheetdeletetable(Store.luckysheetRightHeadClickIs, st_index, ed_index);
     });
     $("#luckysheet-delRows").click(function (event) {
         $("#luckysheet-rightclick-menu").hide();
@@ -1483,6 +1496,9 @@ export function rowColumnOperationInitial(){
 
         let st_index = Store.luckysheet_select_save[0].row[0], 
             ed_index = Store.luckysheet_select_save[0].row[1];
+		if(!method.createHookFunction("rowDeleteBefore", st_index, ed_index)){
+			return; 
+		}
         luckysheetdeletetable('row', st_index, ed_index);
     })
     $("#luckysheet-delCols").click(function (event) {
