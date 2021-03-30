8b90e1d093d48725e38d80d40e807d26712c42ea
btea <2356281422@qq.com>
2021-03-30 11:43:56 +0800

    edit: filter xss

--- src/global/formula.js
+++ src/global/formula.js
@@ -321,6 +321,10 @@ const luckysheetformula = {
             sheetmanage.changeSheetExec(_this.rangetosheet);
         }
     },
+    xssDeal: function(str) {
+        if (typeof str !== 'string') return str;
+        return str.replace(/<script>/g, '&lt;script&gt;').replace(/<\/script>/, '&lt;/script&gt;');
+    },
     fucntionboxshow: function(r, c) {
 
         if(!checkProtectionCellHidden(r, c, Store.currentSheetIndex)){
@@ -346,7 +350,7 @@ const luckysheetformula = {
                 value = valueShowEs(r, c, d);
             }
         }
-
+        value = this.xssDeal(value);
         _this.oldvalue = value;
         $("#luckysheet-functionbox-cell").html(value);
     },
@@ -3313,11 +3317,11 @@ const luckysheetformula = {
             $editer = $input;
         let value1 = $editer.html(),
             value1txt = $editer.text();
-
+        let xssDeal = this.xssDeal
         setTimeout(function() {
             let value = $editer.text(),
                 valuetxt = value;
-
+            value = xssDeal(value);
             if (value.length > 0 && value.substr(0, 1) == "=" && (kcode != 229 || value.length == 1)) {
                 value = _this.functionHTMLGenerate(value);
                 value1 = _this.functionHTMLGenerate(value1txt);
