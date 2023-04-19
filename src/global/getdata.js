cfc418667e7cb71d2902c3018e4d866d7e10063a
yiwasheng <895721391@qq.com>
2023-04-19 15:40:19 +0800

    去除getdata无用循环, 优化加载速度

--- src/global/getdata.js
+++ src/global/getdata.js
@@ -27,7 +27,7 @@ export function getdatabyselection(range, sheetIndex) {
     }
     else{
         d = editor.deepCopyFlowData(Store.flowdata);
-        cfg = Store.config;    
+        cfg = Store.config;
     }
 
     let data = [];
@@ -57,7 +57,7 @@ export function getdatabyselectionD(d, range) {
     if (range == null || range["row"] == null || range["row"].length == 0) {
         return [];
     }
-    
+
     let dynamicArray_compute = dynamicArrayCompute(Store.luckysheetfile[getSheetIndex(Store.currentSheetIndex)]["dynamicArray"]);
     let data = [];
 
@@ -78,7 +78,7 @@ export function getdatabyselectionD(d, range) {
 
         for (let c = range["column"][0]; c <= range["column"][1]; c++) {
             let value;
-            
+
             if((r + "_" + c) in dynamicArray_compute){
                 value = dynamicArray_compute[r + "_" + c];
             }
@@ -104,7 +104,7 @@ export function getdatabyselectionNoCopy(range) {
 
     for (let r = range["row"][0]; r <= range["row"][1]; r++) {
         let row = [];
-        
+
         if (Store.config["rowhidden"] != null && Store.config["rowhidden"][r] != null) {
             continue;
         }
@@ -118,7 +118,7 @@ export function getdatabyselectionNoCopy(range) {
 
             row.push(value);
         }
-        
+
         data.push(row);
     }
 
@@ -211,8 +211,11 @@ export function datagridgrowth(data, addr, addc, iscallback) {
         rowadd.push(null);
     }
 
-    for (let r = 0; r < data.length; r++) {
-        data[r] = [].concat(data[r].concat(coladd));
+    // 下面循环非常耗时, 在coladd为空时是无用循环
+    if (coladd && coladd.length) {
+        for (let r = 0; r < data.length; r++) {
+            data[r] = [].concat(data[r].concat(coladd));
+        }
     }
 
     for (let r = 0; r < addr; r++) {
@@ -238,7 +241,7 @@ export function getcellFormula(r, c, i, data) {
         cell = getOrigincell(r,c,i);
     }
 
-    
+
     if(cell==null){
         return null;
     }
@@ -364,7 +367,7 @@ export function getFontStyleByCell(cell,checksAF,checksCF, isCheck=true){
                 style += "color: " + checksAF[0] + ";";
             }
             else{
-                style += "color: " + value + ";";  
+                style += "color: " + value + ";";
             }
         }
 
@@ -385,7 +388,7 @@ export function checkstatusByCell(cell, a){
             foucsStatus = "0";
         }
         else{
-            // var  w = window.getSelection(), isInlineEdit=false; 
+            // var  w = window.getSelection(), isInlineEdit=false;
             // if(w.type!="None"){
             //     var range = w.getRangeAt(0);
             //     let startContainer = range.startContainer;
@@ -396,18 +399,18 @@ export function checkstatusByCell(cell, a){
             //         isInlineEdit = true;
             //     }
             // }
-            
-            // if(!isInlineEdit){       
+
+            // if(!isInlineEdit){
             //     if(isInlineStringCell(cell)){
             //         foucsStatus = cell.ct.s[0][a];
             //     }
             //     else{
             //         foucsStatus = foucsStatus[a];
             //     }
-            // }   
-            
+            // }
+
             foucsStatus = foucsStatus[a];
-            
+
             if(foucsStatus == null){
                 foucsStatus = "0";
             }
