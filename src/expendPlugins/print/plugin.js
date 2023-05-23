7f707cb214bdeaa999de72a71e01377e45870334
tonytonychopper123 <40351140+tonytonychopper123@users.noreply.github.com>
2023-05-23 17:59:26 +0800

    xiugai
    jiang

--- src/expendPlugins/print/plugin.js
+++ src/expendPlugins/print/plugin.js
@@ -24,7 +24,7 @@ function print(options, config, isDemo) {
         const link = document.createElement("link");
         link.setAttribute("rel", "stylesheet");
         link.setAttribute("type", "text/css");
-        link.setAttribute("href", "/expendPlugins/print/print.css");
+        link.setAttribute("href", "./expendPlugins/print/print.css");
         document.head.appendChild(link);
     }
 }
